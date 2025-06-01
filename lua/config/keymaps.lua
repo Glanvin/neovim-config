7e495efd5d2d6fa3884da214d358dff3900044f3
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-06-01 16:20:53 +0800

    New keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit window" })
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("n", "<leader>jk", vim.cmd.Ex, { desc = "Ex" })
+vim.keymap.set("n", "<leader>mn", vim.cmd.exit, { desc = "exit" })
 --Telescope keymaps
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
@@ -11,4 +12,10 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fgf', builtin.git_files, { desc = 'Telescope find git files' })
 --Neotree
-vim.keymap.set("n", "<leader>ew", "<Cmd>Neotree reveal<CR>")
+vim.keymap.set('n', '<leader>e', ':Neotree reveal<CR>')
+vim.keymap.set('n', '<leader>c', ':Neotree close<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>o', ':Neotree focus<CR>', { noremap = true, silent = true })
+--Undotree
+vim.keymap.set('n', '<leader>m', vim.cmd.UndotreeToggle)
+--Fugitive
+vim.keymap.set('n', '<leader>nm', vim.cmd.Git)
