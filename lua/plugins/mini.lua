610e198abd0daafb8cf4fc10d5a4b42425f94e34
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-06-01 21:19:51 +0800

    Remove blank space

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,11 @@
-return { 
-	'echasnovski/mini.nvim',
-	version = '*',
-	config = function()
-		require("mini.animate").setup()
-		require("mini.statusline").setup()
-		require("mini.indentscope").setup()
-		require("mini.comment").setup()
-		require("mini.pairs").setup()
-		
-	end
+return {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+        require("mini.animate").setup()
+        require("mini.statusline").setup()
+        require("mini.indentscope").setup()
+        require("mini.comment").setup()
+        require("mini.pairs").setup()
+    end
 }
