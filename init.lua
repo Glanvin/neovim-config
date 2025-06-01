e3c36606a67e6a9906cc3e22bfe21908c63f02c1
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-06-01 15:52:57 +0800

    Added mn

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
+vim.cmd([[
+  filetype plugin indent on
+  syntax on
+]])
 vim.g.mapleader = " "
 
 require("config.lazy")
 require("config.keymaps")
+require("config.lsp")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
